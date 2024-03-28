a77a8d20adc63a496cfd52d0052be0a7625ffe31
Your Name <you@example.com>
2024-03-27 19:41:34 -0700

    Database and env config set

--- api/index.js
+++ api/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const connectDB = require('./config/database');
 require('dotenv').config();
 
 
 const app = express();
 const port = 3000;
+
+connectDB();
 
 app.get('/', (req, res) => {
   res.send('¡Hola, mundo!');
